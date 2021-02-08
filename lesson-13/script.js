37c4dd77498ced557f52d7e8a943c0c053a2fc24
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-02-09 01:44:30 +0600

    made buttons to increase and decrease the number in the title

--- lesson-13/script.js
+++ lesson-13/script.js
@@ -20,11 +20,7 @@ example2.style.padding = "15px";
 example2.style.border = "solid black 2px";
 example2.style.backgroundColor = "green";
 example2.style.margin = "10px";
-let size = 20;
-document.getElementById('h1').style.fontSize = size ;
-example2.addEventListener('click', function(){
-    document.getElementById("h1").style.fontSize = size++;
-})
+
 
 
 let example3 = document.querySelector('#example3');
@@ -32,26 +28,82 @@ example3.style.padding = "15px";
 example3.style.border = "solid black 2px";
 example3.style.backgroundColor = "red";
 example3.style.margin = "10px";
-example3.addEventListener('click', function(){
-    header.style.fontSize = header--
-})
 
 let example4 = document.querySelector('#example4');
 example4.style.padding = "15px";
 example4.style.border = "solid black 2px";
 example4.style.backgroundColor = "green";
 example4.style.margin = "10px";
-example4.document.addEventListener('click', function(event) {
-
-    if (event.target.dataset.counter != undefined) {
-      event.target.value++;
-    }
-    
-    document.getElementById("h1").innerText = event;
-});
+example4.addEventListener('click', function () {
+    let number = parseInt(h1.innerText);
+    number++;
+    h1.innerText = number;
+})
 
 let example5 = document.querySelector('#example5');
 example5.style.padding = "15px";
 example5.style.border = "solid black 2px";
 example5.style.backgroundColor = "red";
 example5.style.margin = "10px";
+example5.addEventListener('click', function () {
+    let number = parseInt(h1.innerText);
+    number--;
+    h1.innerText = number;
+})
+
+
+let input = document.getElementById("#text")
+input.addEventListener('change', function () {
+    document.getElementById("#h1").innerText = input.value;
+})
+
+//Alinur ver
+
+
+// let click = document.querySelector("#click");
+// let h1 = document.querySelector("h1");
+// let sayHello = document.querySelector("#hello");
+// let input = document.querySelector("#chenge");
+
+
+// click.addEventListener('click', function () {
+//     h1.innerText = "Hello java scripts"
+// })
+// sayHello.addEventListener('click', function () {
+//     h1.innerText = "Hello User"
+// })
+// input.addEventListener('change', function () {
+//     h1.innerText = input.value;
+// })
+// color.querySelector('change',function (){
+//     h1.style.color=color.value
+// })
+/*
+let example1 = document.querySelector("#example1");
+example1.style.backgroundColor = 'rgb(253, 76, 45)';
+example1.style.color = 'balck';
+example1.style.fontSize = '20px';
+example1.style.border = '2px solid yellow'
+
+
+let h1 = document.querySelector('h1');
+example2.addEventListener('click', function () {
+    h1.style.fontSize = (Infinity);
+
+})
+example4.addEventListener('click', function () {
+    let number =  (h1.innerText);
+    number++;
+    h1.innerText = number;
+})
+
+example5.addEventListener('click', function () {
+    let number = parseInt(h1.innerText);
+    number--;
+    h1.innerText = number;
+})
+let input = document.querySelector('#example2');
+input.addEventListener('click',function(){
+    h1.innerText=input.value;
+})
+*/
