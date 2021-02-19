12c3834c248f1ce997636482624b3d692188352d
Aman Kalkanbaev <kalkanbaev5@gmail.com>
2021-02-19 19:52:07 +0600

    started write js code

---
+++ lesson-18/script.js
@@ -0,0 +1,44 @@
+let nameSelector = document.querySelector('#name');
+let messege = document.querySelector('#messege');
+let color = document.querySelector('#color');
+let button = document.querySelector('#button');
+let chat = document.querySelector('#chat');
+
+button.addEventListener('click', function(){
+    let div = document.createElement("div");
+    let span = document.createElement("span");
+    let spanSec = document.createElement("span");
+    span.innerText = nameSelector.value + ": "
+    spanSec.innerText = messege.value
+    span.style.color = color.value
+    chat.append(div);
+    div.append(span);
+    div.append(spanSec);
+    fetch('https://it-academy-ajax-table-14a18-default-rtdb.firebaseio.com/chat.json', {
+        method: "POST",
+        body: JSON.stringify({
+            name: nameSelector.value,
+            messege: messege.value,
+            color: color.value
+        })
+
+    })
+
+});
+fetch('https://it-academy-ajax-table-14a18-default-rtdb.firebaseio.com/chat.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for (const key in data) {
+            let div = document.createElement("div");
+            let span = document.createElement("span");
+            let spanSec = document.createElement("span");
+            span.innerText = data[key].name + ": "
+            spanSec.innerText = data[key].messege
+            span.style.color = data[key].color
+            chat.append(div);
+            div.append(span);
+            div.append(spanSec);
+        }
+    })
